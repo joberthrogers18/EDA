11b0de8e0b746c610ed70eed776f48e9d86d7d17
Joberth Rogers <joberth.rogers18@gmail.com>
2018-03-31 14:12:57 -0300

    Adding function count grades
    this function receive a vector with the students grade and add the variables approved and disapproved.

--- problema2.c
+++ problema2.c
@@ -4,6 +4,7 @@
 #define TAMANHO 10
 
 void recebe_notas (float *notas, int *APR, int tamanho);
+void contador_notas (int *APR, int tamanho, int *aprovados, int *reprovados);
 
 int main (){
 
@@ -19,7 +20,11 @@ int main (){
     }
 
     recebe_notas(notas, APR, TAMANHO);
+    contador_notas(APR, TAMANHO, &aprovados, &reprovados);
 
+    printf("aprovados: %d ",aprovados);
+
+    printf("reprovados: %d ",reprovados);
 
 
     return 0;
@@ -33,6 +38,20 @@ void recebe_notas ( float *notas, int *APR, int tamanho){
             APR[contador] = 1;
         else
             APR[contador] = 0;
+    }
+
+}
+
+void contador_notas (int *APR, int tamanho, int  *aprovados, int *reprovados){
+    int contador = 0;
+
+    for (contador = 0; contador < tamanho; contador++){
+        if(APR[contador] == 1)
+            *aprovados += 1;
+
+        else if(APR[contador] == 0)
+            *reprovados += 1;
+
     }
 
 }
