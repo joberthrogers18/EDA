5a7903e0fa9704a4a70ac5ae9202e69b4a4bfe93
Joberth Rogers <joberth.rogers18@gmail.com>
2018-03-31 00:46:54 -0300

    Make progress in program
    Add a function pass by parameter vector pointer and goes right'

--- problema1.c
+++ problema1.c
@@ -3,28 +3,50 @@
 
 #define TAMANHO 10
 
+int nivel_agitacao = 0;     // o neurônio já começa inibido
+
+void fneuronio(float *entradas, float *pesos, int tamanho_vetor, float limiar);
+
 int main(){
 
   float entradas[TAMANHO];
-  float peso[TAMANHO];
+  float pesos[TAMANHO];
   float limiar_T;
   int contador = 0;
 
   for(contador = 0; contador < TAMANHO; contador++){
-    printf("Entre com o valor do %dº entrada: ", contador );
-    scanf("%d", &entrada[contador]);
+    printf("Entre com o valor do %dº entrada: ", (contador + 1) );
+    scanf("%f", &entradas[contador]);
   }
 
   printf("\n");
 
   for(contador = 0; contador < TAMANHO; contador++){
-    printf("Entre com o valor do %dº peso: ", contador );
-    scanf("%d", &peso[contador]);
+    printf("Entre com o valor do %dº peso: ", (contador + 1));
+    scanf("%f", &pesos[contador]);
   }
 
-  printf("Digite o valor do limiar T desejado: ");
-  scanf("%d\n", &limiar_T);
+  printf("\n");
 
+  printf("Digite o valor do limiar T: ");
+  scanf("%f", &limiar_T);
+
+  printf("\n" );
+
+  fneuronio(entradas, pesos, TAMANHO, limiar_T);
 
   return 0;
+}
+
+void fneuronio (float *entradas, float *pesos, int tamanho_vetor, float limiar){
+
+  int contador = 0;
+  float media_ponderada = 0.0;
+
+  for(contador = 0; contador < tamanho_vetor; contador++){
+    media_ponderada += *(entradas + contador) * *(pesos + contador);
+  }
+
+  printf("%f\n", media_ponderada );
+
 }
