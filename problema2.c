3e86dd87193cb581676f3b89c311841734a32a7d
Joberth Rogers <joberth.rogers18@gmail.com>
2018-03-31 15:04:18 -0300

    Adding function and features
    function make calculation bout percent approved and disapproved students and print this in screen

--- problema2.c
+++ problema2.c
@@ -5,6 +5,7 @@
 
 void recebe_notas (float *notas, int *APR, int tamanho);
 void contador_notas (int *APR, int tamanho, int *aprovados, int *reprovados);
+int percent_aprov (int *aprovados, int *reprovados, float *perc_aprov, float *perc_reprov);
 
 int main (){
 
@@ -13,6 +14,8 @@ int main (){
     int APR[TAMANHO];
     int aprovados = 0;
     int reprovados = 0;
+    float perc_aprov = 0.0;
+    float perc_reprov = 0.0;
 
     for (contador = 0; contador < TAMANHO; contador++){
         printf("Digite a %dº nota: ", contador + 1);
@@ -22,10 +25,28 @@ int main (){
     recebe_notas(notas, APR, TAMANHO);
     contador_notas(APR, TAMANHO, &aprovados, &reprovados);
 
-    printf("aprovados: %d ",aprovados);
+    printf("\n");
 
-    printf("reprovados: %d ",reprovados);
+    printf("Quantidade de aprovados: %d \n",aprovados);
 
+    printf("Quantidade de reprovados: %d \n",reprovados);
+
+    printf("\n");
+
+    percent_aprov (&aprovados, &reprovados, &perc_aprov, &perc_reprov);
+
+    printf("Percentual de aprovados: %.1f%%\n", perc_aprov);
+
+    printf("Percentual de reprovados: %.1f%%\n", perc_reprov);
+
+    printf("\n");
+
+    if (percent_aprov (&aprovados, &reprovados, &perc_aprov, &perc_reprov) == 1)
+        printf("A maioria da turma foi APROVADA\n");
+    else
+        printf("A maioria da turma foi REPROVADA\n");
+
+    printf("\n");
 
     return 0;
 }
@@ -54,4 +75,14 @@ void contador_notas (int *APR, int tamanho, int  *aprovados, int *reprovados){
 
     }
 
+}
+
+int percent_aprov (int *aprovados, int *reprovados, float *perc_aprov, float *perc_reprov){
+    *perc_aprov = ((*aprovados)  / 10.00) * 100.00 ;
+    *perc_reprov = (*reprovados / 10.00) * 100.00;
+
+    if (*aprovados > 5)
+        return 1;
+    else
+        return 0;
 }
