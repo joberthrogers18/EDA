916871df9ba49fa63da14e00416cc725a125699a
Joberth Rogers <joberth.rogers18@gmail.com>
2018-04-19 14:21:57 -0300

    Linhas e colunas do arquivo
    quantidade de linhas e colunas dada pela matriz do arquivo

--- Projeto2/projeto2.c
+++ Projeto2/projeto2.c
@@ -5,32 +5,37 @@
 int main (int argc, char *argv[])
 {
     FILE *fp;
-    int numero;
-    char comma;
+    int numero,linha = 0, col = 0;          // declaração de caracteres
+    char pegar_c , palavra , p_virg = ';';
 
     if (argc!=2) {
-        printf("You forgot to enter the filename.\n");
+        printf("Você esqueceu de importar o arquivo.\n");      // mensagem caso o arquivo não seja indicado
         exit(1);
     }
 
-    fp=fopen("test.txt", "r");
+    fp=fopen("test.txt", "r");          // abri o arquivo
 
     if(fp==NULL) {
-        printf("Cannot open file.\n");
+        printf("Não foi possivel abri o arquivo.\n");        // caso o arquivo não exista
         exit(2);
     }
 
-    fscanf (fp, "%d%c", &numero, &comma);
-
-    while (!feof(fp)) {
+    while ((palavra=fgetc(fp))!=EOF) {
         // to do: implementar o contador e verificar como ler o número sem ponto e virgula
-        if(){
-          printf("%d\n", numero); /* print on screen */
-          fscanf(fp, "%d%c", &numero, &comma);
+
+        if(palavra == '\n')
+        {
+          linha++;           // verficando quantas linhas no arquivo
         }
-        printf("%d ", numero); /* print on screen */
-        fscanf(fp, "%d%c", &numero, &comma);
+        else if(linha == 1 && palavra == p_virg){
+          col++;         // verificando quantas colunas no arquivo
+        }
+
     }
+
+
+    printf("%d\n", linha );
+    printf("%d\n", col );
 
     fclose(fp);
     return 0;
