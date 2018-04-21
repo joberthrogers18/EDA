8895e3020dea70d32a709be7a8edc1b5e366cd54
Joberth Rogers <joberth.rogers18@gmail.com>
2018-04-21 18:51:28 -0300

    Verificação do do ilbp
    função para verificar e retorna o binario
    
    ainda incompleta

--- Projeto2/projeto2.c
+++ Projeto2/projeto2.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+void mediamatriz(int **, int, int);
+
 int main (int argc, char *argv[])
 {
     FILE *fp;
     int numero,linha = 0, col = 0;          // declaração de caracteres
     char pegar_p , palavra , p_virg = ';';
-    int i,j, **mat;
+    int i,j, **mat, cont = 0, *media;
 
-    if (argc!=2) {
+    /*if (argc!=2) {
         printf("Você esqueceu de importar o arquivo.\n");      // mensagem caso o arquivo não seja indicado
         exit(1);
-    }
+    }*/
 
     fp=fopen("test.txt", "r");          // abri o arquivo
 
@@ -36,6 +38,8 @@ int main (int argc, char *argv[])
 
     col++;
 
+    media = (int*)malloc(((col - 1 )*(linha - 1))*sizeof(int));
+
     mat = (int**)malloc(linha*sizeof(int *));
 
     for(i = 0; i<linha; i++)                         //declaração da matriz
@@ -54,6 +58,15 @@ int main (int argc, char *argv[])
       }
     }
 
+    cont = 0;
+
+    for(i = 1; i<linha - 1; i++){
+      for(j = 1; j<col - 1; j++){     //verificar toda a posição da matriz com seus vizinhos
+          mediamatriz(mat, i, j);
+          cont++;
+        }
+      }
+
     for (i=0;i<linha;i++){
       for (j=0;j<col;j++){
           printf("%d ",*(*(mat+i)+j));    // imprime a matriz
@@ -66,11 +79,41 @@ int main (int argc, char *argv[])
 
       free(mat);    // libera o vetor de ponteiros
 
-
+      free(media);
 
     printf("numero de linhas: %d\n", linha );
     printf("numero de colunas: %d\n", col );
 
     fclose(fp);
     return 0;
+}
+
+void mediamatriz(int **M, int L, int C){
+  int i, j, bin[3][3], contl = 0, contc = 0;
+
+  float soma = 0.0;
+
+    for(i = L - 1; i<L + 2; i++){
+      for(j = C - 1; j<C + 2; j++){
+        soma += *(*(M+i)+j);
+      }
+  }
+
+  soma = soma / 9.0;
+
+
+
+for(i = L - 1; i<L + 2; i++){
+        for(j = C - 1; j<C + 2; j++){
+          if(soma >= *(*(M+i)+j)){
+            printf("%d,",1);
+          }
+          else{
+            printf("%d,",0);
+          }
+        }
+    }
+
+    printf("\n");
+
 }
