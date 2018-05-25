4f264ca42d482b8ff7d0ca5cf44a3d1df1671b8b
Joberth Rogers <joberth.rogers18@gmail.com>
2018-05-25 17:41:50 -0300

    Adding struct
    form student and declaration

--- others/fila_estatica.c
+++ others/fila_estatica.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
 #include "fila_estatica.h"
+
+struct fila {
+  int inicio, fim, qtd;
+  struct aluno dados[MAX];
+};
+
+int main(){
+  
+}
--- others/fila_estatica.h
+++ others/fila_estatica.h
@@ -3,7 +3,10 @@
 
 #define MAX 100  //tamanho maximo da fila
 struct aluno{
-
+  int matricula;
+  char nome[100];       //struct para aluno
+  float n1, n2, n3;
 };
+  typedef struct fila Fila; // definindo struct fila
 
 #endif
