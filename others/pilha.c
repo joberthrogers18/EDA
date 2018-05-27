bba97019a03b3612ab844c909752c490ca440c86
Joberth Rogers <joberth.rogers18@gmail.com>
2018-05-26 21:28:58 -0300

    First steps in stack

---
+++ others/pilha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_PILHA 10
+
+
+typedef struct {
+  int valor[MAX_PILHA];
+  int topo;
+}Tpilha;
+
+typedef Tpilha *Ppilha;
+
+Ppilha criar_pilha(){
+  
+}
+
+int main(){
+
+}
