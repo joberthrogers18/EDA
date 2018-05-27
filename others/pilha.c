a6acd07b698bd5083624476ccb70401c910d586b
Joberth Rogers <joberth.rogers18@gmail.com>
2018-05-26 21:52:20 -0300

    functions pop peek and free

--- others/pilha.c
+++ others/pilha.c
@@ -16,7 +16,7 @@ Ppilha criar_pilha(){
     return p;
 }
 
-int push (Ppilha p, int val){
+int push (Ppilha p, int val){ // acrescenta valor na pilha
   if(p == NULL)
     return -1; //pilha não inicializada
   if(p->topo >= MAX_PILHA-1)
@@ -37,6 +37,20 @@ int pop(Ppilha p, int *res){ // recebe a pilha e o valor que será armazenado o 
   p->topo--;
   return 1;
 }
+
+int peek(pPilha p){  // apenas retorna o topo da pilha
+  return p->valor[p->topo];
+}
+
+void libera(Ppilha p){
+  int i;
+  for(i = 0; i < p->topo; i++){
+    free(p->valor[i]);     // libera todos os valores da pilha
+  }
+  free(p); // libera o ponteiro
+}
+
+
 
 int main(){
 
