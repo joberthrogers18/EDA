74a1b57435a4ca50a6f98671a0661feaf495cfca
Joberth Rogers <joberth.rogers18@gmail.com>
2018-05-31 16:35:00 -0300

    define struct

--- Projeto4/main.c
+++ Projeto4/main.c
@@ -2,13 +2,44 @@
 #include <stdlib.h>
 #include <time.h>
 
+struct voo{
+      char codigo[7] ;
+      char tipo;//A ou D;
+      int comb;
+      struct voo* prox ;
+      };
+typedef struct voo Voo;
+
+int num_voo()
+{
+  int voo;
+  voo = 20 + (rand()%44);
+
+  return voo;
+}
+
+int num_prioridade()
+{
+  int prioridade;
+  prioridade = (rand()%12);
+
+  return prioridade;
+}
+
 int main(void)
 {
-     int x;
-     srand(time(NULL));
+    int voo, prioridade;
+    srand(time(NULL));
 
-    x = 20 + (rand()%44);
-    printf("%d ", x);
+    voo = num_voo();
+
+
+    for (int i = 0; i < 64; i++) {
+      printf("%s ", cod_voo[i]);
+
+    }
+
+
 
      return 0;
 }
