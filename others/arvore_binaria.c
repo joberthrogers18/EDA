ce0cab5b11082ccf49564e2faa40373a15155bcc
Joberth Rogers <joberth.rogers18@gmail.com>
2018-06-10 14:04:34 -0300

    Method for detruct tree

--- others/arvore_binaria.c
+++ others/arvore_binaria.c
@@ -4,17 +4,19 @@
 struct NO{
   int info;
   struct NO* esq;
-  struct No* dir;
+  struct NO* dir;
 };
 
 typedef struct NO* ArvBin; //é um ponteiro que aponta para minha struct nó
 
 ArvBin* cria_ArvBin(); // criação de uma arvore binaria null
-
+void libera_ArvBin(ArvBin* raiz);
+void libera_NO(struct NO* no);
 
 int main(int argc, char const *argv){
-  ArvBin* raiz; // criando a raiz da arvore
+  ArvBin* raiz = cria_ArvBin();; // criando a raiz da arvore
 
+  libera_ArvBin(raiz);
 
   return 0;
 }
@@ -25,4 +27,21 @@ ArvBin* cria_ArvBin(){
     *raiz = NULL;
   }
   return raiz;
+}
+
+void libera_ArvBin(ArvBin* raiz){
+  if(raiz == NULL)   // se não tem nada alocado
+    return;
+  libera_NO(*raiz);
+  free(raiz); // libera raiz
+}
+
+void libera_NO(struct NO* no){
+  if(no == NULL)
+    return;
+  libera_NO(no->esq);  // corre recursivamente o nó da esquerda
+  libera_NO(no->dir);  // corre recursivamente o nó da esquerda
+  free(no);
+  no = NULL;
+
 }
