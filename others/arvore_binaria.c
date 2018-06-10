7f04e931ea5897ed457610cba2f46b8306f8a4fb
Joberth Rogers <joberth.rogers18@gmail.com>
2018-06-10 13:49:05 -0300

    Adding fuction create the root of tree

--- others/arvore_binaria.c
+++ others/arvore_binaria.c
@@ -7,7 +7,22 @@ struct NO{
   struct No* dir;
 };
 
+typedef struct NO* ArvBin; //é um ponteiro que aponta para minha struct nó
+
+ArvBin* cria_ArvBin(); // criação de uma arvore binaria null
+
+
 int main(int argc, char const *argv){
+  ArvBin* raiz; // criando a raiz da arvore
+
 
   return 0;
+}
+
+ArvBin* cria_ArvBin(){
+  ArvBin* raiz = (ArvBin*) malloc(sizeof(ArvBin));
+  if(raiz != NULL){ // se deu certo a alocação o ponteiro para raiz recebe null
+    *raiz = NULL;
+  }
+  return raiz;
 }
