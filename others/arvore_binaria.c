2f818d3aa148e8b0561be48fe672c238c857ebc1
Joberth Rogers <joberth.rogers18@gmail.com>
2018-06-10 19:52:34 -0300

    Function of remotion

--- others/arvore_binaria.c
+++ others/arvore_binaria.c
@@ -16,7 +16,11 @@ int estaVazia_ArvBin(ArvBin* raiz);
 int altura_ArvBin(ArvBin* raiz);
 int totalNO_ArvBin(ArvBin* raiz);
 void preOrdem_ArvBin(ArvBin* raiz);
-void preOrdem_ArvBin(ArvBin* raiz);
+void emOrdem_ArvBin(ArvBin* raiz);
+void posOrdem_ArvBin(ArvBin* raiz);
+int insere_ArvBin(ArvBin* raiz, int valor);
+int remove_ArvBin(ArvBin* raiz, int valor);
+struct NO* remove_atual(struct NO* atual);
 
 int main(int argc, char const *argv){
   ArvBin* raiz = cria_ArvBin();; // criando a raiz da arvore
@@ -101,4 +105,122 @@ void emOrdem_ArvBin(ArvBin* raiz)
     printf("%d\n", (*raiz)->info);
     emOrdem_ArvBin(&((*raiz)->dir));
   }
+}
+
+void posOrdem_ArvBin(ArvBin* raiz)
+{
+  if(raiz == NULL)
+    return;
+  if(*raiz != NULL){
+    emOrdem_ArvBin(&((*raiz)->esq));
+    emOrdem_ArvBin(&((*raiz)->dir));
+    printf("%d\n", (*raiz)->info);
+  }
+}
+
+int insere_ArvBin(ArvBin* raiz, int valor)
+{
+  if(raiz == NULL)
+    return 0;
+  struct NO* novo;
+  novo = (struct NO*)malloc(sizeof(struct NO));
+  if(novo = NULL)
+    return 0;
+  novo->info = valor;
+  novo->dir = NULL;
+  novo->esq = NULL;
+
+  if(*raiz == NULL)
+  {
+    *raiz = novo;
+  }
+  else
+  {
+    struct NO* atual =  *raiz;
+    struct NO* ant =  NULL;
+
+    while(atual != NULL){     // navega pela arvoreaté chegar numa folha
+        ant = atual;
+
+        if(valor == atual->info)
+        {
+          free(novo);
+          return 0; // elemento já existe
+        }
+        if(valor > atual->info)
+          atual = atual->dir;
+        else
+          atual = atual->esq;
+    }
+
+    if(valor > atual->info)   // insere como filho desse nó  folha
+      atual->dir = novo;
+    else
+      atual->esq = novo;
+  }
+
+  return 1;
+}
+
+int remove_ArvBin(ArvBin* raiz,int valor)
+{
+  /*
+    FUNÇÃO RESPONSAVEL PELA BUSCA
+    DO NÓ A SER REMOVIDo
+  */
+
+  if(raiz == NULL) return 0;
+  struct NO* atual =  *raiz;
+  struct NO* ant =  NULL;
+  while(atual != NULL)
+  {
+    if(valor ==  atual->info){
+      if(atual ==  *raiz)
+        *raiz = remove_atual(atual);
+      else{
+        if(ant->dir == atual)
+          ant->dir = remove_atual(atual);
+        else
+          ant->esq = remove_atual(atual);
+      }
+      return 1;
+    }
+    ant = atual;
+
+    if(valor > atual->info)
+      atual= atual->dir;
+    else
+      atual = atual->esq;
+  }
+
+}
+
+struct NO* remove_atual(struct NO* atual)
+{
+  /*
+    FUNÇÃO RESPONSAVEL POR TRATAR
+    OS 3 TIPOS DE REMOÇÃO
+  */
+
+  struct NO *no1, *no2;
+   if(atual->esq == NULL){    // sem filho da esquerda aponta para filha da direita(trata nó folha e nó com um filho)
+     no2 = atual->dir;
+     free(atual);
+     return no2;
+   }
+   no1 = atual;
+   no2 = atual->esq;
+   while (no2->dir != NULL) {  //procura filho mais a direita na subarvore da esquerda
+     no1 = no2;
+     no2 = no2->dir;
+   }
+
+   if (no1 != atual){
+     no1->dir = no2->esq;
+     no2->esq = atual->esq;  //procura filho mais a direita na subarvore da esquerda para o lugar do nó removido
+   }
+   no2->dir = atual->dir;
+   free(atual);
+   return no2;
+
 }
